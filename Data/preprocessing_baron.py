2337cd97e9a6299cedd2804160f4464a940caf3b
LiShenghao813 <1477058059@qq.com>
2022-10-24 12:01:21 +0800

    Datasets

--- Data/preprocessing_baron.py
+++ Data/preprocessing_baron.py
@@ -8,14 +8,25 @@ Created on Thu Oct 13 10:51:12 2022
 import anndata as ad
 import pandas as pd
 import numpy as np
-
-
-human1 = pd.read_csv('./GSM2230757_human1_umifm_counts.csv')
-human2 = pd.read_csv('./GSM2230758_human2_umifm_counts.csv')
-human3 = pd.read_csv('./GSM2230759_human3_umifm_counts.csv')
-human4 = pd.read_csv('./GSM2230760_human4_umifm_counts.csv')
-mouse1 = pd.read_csv('./GSM2230761_mouse1_umifm_counts.csv')
-mouse2 = pd.read_csv('./GSM2230762_mouse2_umifm_counts.csv')
+import zipfile
+def unzip_file(dataname):
+    zip_name = './Baron/' + dataname + '.zip'
+    dst_dir = './Baron' 
+    r = zipfile.is_zipfile(zip_name)
+    if r:
+        fz = zipfile.ZipFile(zip_name, 'r')
+        for file in fz.namelist():
+            fz.extract(file, dst_dir)
+    else:
+        print('This is not zip')
+for i in ['Baron1','Baron2','Baron3','Baron4','Baron mouse1','Baron mouse2']:
+    unzip_file(i)
+human1 = pd.read_csv('./Baron/GSM2230757_human1_umifm_counts.csv')
+human2 = pd.read_csv('./Baron/GSM2230758_human2_umifm_counts.csv')
+human3 = pd.read_csv('./Baron/GSM2230759_human3_umifm_counts.csv')
+human4 = pd.read_csv('./Baron/GSM2230760_human4_umifm_counts.csv')
+mouse1 = pd.read_csv('./Baron/GSM2230761_mouse1_umifm_counts.csv')
+mouse2 = pd.read_csv('./Baron/GSM2230762_mouse2_umifm_counts.csv')
 human1 = human1.dropna()
 human2 = human2.dropna()
 human3 = human3.dropna()
@@ -46,8 +57,8 @@ celltype1_4 = np.asarray(human4['assigned_cluster'])
 adata1_4.obs['celltype'] = celltype1_4
 
 
-adata1_1.write_h5ad('./Pancreas_human1.h5ad')
-adata1_2.write_h5ad('./Pancreas_human2.h5ad')
-adata1_3.write_h5ad('./Pancreas_human3.h5ad')
-adata1_4.write_h5ad('./Pancreas_human4.h5ad')
-adata2.write_h5ad('./Pancreas_mouse.h5ad')
+adata1_1.write_h5ad('./AnnData/Pancreas_human1.h5ad')
+adata1_2.write_h5ad('./AnnData/Pancreas_human2.h5ad')
+adata1_3.write_h5ad('./AnnData/Pancreas_human3.h5ad')
+adata1_4.write_h5ad('./AnnData/Pancreas_human4.h5ad')
+adata2.write_h5ad('./AnnData/Pancreas_mouse.h5ad')
